cdafe56c9047400053acf2ed9b69ecac17ebdd64
Timmifixedit <tim@luchterhand.org>
2020-06-02 17:33:12 +0200

    Primzahltest nützlich gemacht

--- Blatt6/primeTest.py
+++ Blatt6/primeTest.py
@@ -1,44 +1,67 @@
 import random
+import sys
 
 
-def miller_rabin_prime_test(n, a=None):
+def miller_rabin_prime_test(n, a=None, verbose=False):
     if a is None:
         a = random.randint(1, n - 1)
 
     d = last_d = int(1)
     binary = [int(c) for c in bin(n - 1)[2:]]
-    print("Binary representation of n - 1 = %d is %s" % (n - 1, binary))
-    print("Initialize d and dd to 1")
-    print("Starting prime test with a = %d" % a)
+    if verbose:
+        print("Binary representation of n - 1 = %d is %s" % (n - 1, binary))
+        print("Initialize d and dd to 1")
+        print("Starting prime test with a = %d" % a)
+
     counter = 0
     for b in binary:
         counter += 1
-        print("Iteration %d" % counter)
+        if verbose:
+            print("Iteration %d" % counter)
+
         d = d * d % n
-        print("Calculate %d * %d mod %d = %d" % (last_d, last_d, n, d))
-        print("Is %d a square root of %d mod %d and not 1 or -1 (%d)?" % (last_d, d, n, n - 1))
+        if verbose:
+            print("Calculate %d * %d mod %d = %d" % (last_d, last_d, n, d))
+            print("Is %d a square root of %d mod %d and not 1 or -1 (%d)?" % (last_d, d, n, n - 1))
+
         if d == 1 and last_d != 1 and last_d != n - 1:
-            print("Yes. %d is definitely no prime number" % n)
+            if verbose:
+                print("Yes. %d is definitely no prime number" % n)
+
             return False
-        print("No, continuing...")
+
+        if verbose:
+            print("No, continuing...")
+
         if b == 1:
-            print("Square and multiply: %d * %d mod %d = %d" % (d, a, n, d * a % n))
+            if verbose:
+                print("Square and multiply: %d * %d mod %d = %d" % (d, a, n, d * a % n))
+
             d = d * a % n
 
         last_d = d
+    if verbose:
+        print("After last iteration d has the value %d" % d)
 
-    print("After last iteration d has the value %d" % d)
     if d == 1:
-        print("Since %d is 1, %d is most likely a prime number" % (d, n))
+        if verbose:
+            print("Since %d is 1, %d is most likely a prime number" % (d, n))
+
         return True
-    else:
+    elif verbose:
         print("Since %d is not 1, %d is definitely not a prime number" % (d, n))
+
+    return False
 
 
 def main():
-    print(miller_rabin_prime_test(n=65, a=8))
-    print("-----------------------------")
-    print(miller_rabin_prime_test(n=65, a=12))
+    if len(sys.argv) == 2:
+        n = int(sys.argv[1])
+        print(miller_rabin_prime_test(n))
+    else:
+        print(miller_rabin_prime_test(n=65, a=8, verbose=True))
+        print("-----------------------------")
+        print(miller_rabin_prime_test(n=65, a=12, verbose=True))
 
 
 if __name__ == '__main__':
